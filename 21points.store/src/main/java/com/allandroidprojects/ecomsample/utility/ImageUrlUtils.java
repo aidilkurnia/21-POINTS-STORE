d7615d6174c712e7bfb7a7f7f7d252fe2e9c008b
aidilkurnia <aidilkurniaa@gmail.com>
2017-12-04 13:53:00 +0800

    version 1.0

---
+++ 21points.store/src/main/java/com/allandroidprojects/ecomsample/utility/ImageUrlUtils.java
@@ -0,0 +1,129 @@
+package com.allandroidprojects.ecomsample.utility;
+
+import java.util.ArrayList;
+
+/**
+ * Created by 06peng on 2015/6/24.
+ */
+public class ImageUrlUtils {
+    static ArrayList<String> wishlistImageUri = new ArrayList<>();
+    static ArrayList<String> cartListImageUri = new ArrayList<>();
+
+    public static String[] getImageUrls() {
+        String[] urls = new String[] {
+                "https://static.pexels.com/photos/76093/pexels-photo-76093.jpeg",
+                "https://static.pexels.com/photos/46239/salmon-dish-food-meal-46239.jpeg",
+                "https://static.pexels.com/photos/376464/pexels-photo-376464.jpeg",
+                "https://static.pexels.com/photos/461198/pexels-photo-461198.jpeg"
+        };
+        return urls;
+    }
+
+    public static String[] getOffersUrls() {
+        String[] urls = new String[]{
+                "https://static.pexels.com/photos/76093/pexels-photo-76093.jpeg",
+                "https://static.pexels.com/photos/46239/salmon-dish-food-meal-46239.jpeg",
+                "https://static.pexels.com/photos/376464/pexels-photo-376464.jpeg",
+                "https://static.pexels.com/photos/461198/pexels-photo-461198.jpeg"
+        };
+        return urls;
+    }
+
+    public static String[] getHomeApplianceUrls() {
+        String[] urls = new String[]{
+                "https://static.pexels.com/photos/76093/pexels-photo-76093.jpeg",
+                "https://static.pexels.com/photos/46239/salmon-dish-food-meal-46239.jpeg",
+                "https://static.pexels.com/photos/376464/pexels-photo-376464.jpeg",
+                "https://static.pexels.com/photos/461198/pexels-photo-461198.jpeg"
+        };
+        return urls;
+    }
+
+    public static String[] getElectronicsUrls() {
+        String[] urls = new String[]{
+                "https://static.pexels.com/photos/204611/pexels-photo-204611-medium.jpeg",
+                "https://static.pexels.com/photos/214487/pexels-photo-214487-medium.jpeg",
+                "https://static.pexels.com/photos/168575/pexels-photo-168575-medium.jpeg",
+                "https://static.pexels.com/photos/213384/pexels-photo-213384-medium.jpeg",
+                "https://static.pexels.com/photos/114907/pexels-photo-114907-medium.jpeg",
+                "https://static.pexels.com/photos/185030/pexels-photo-185030-medium.jpeg",
+                "https://static.pexels.com/photos/133579/pexels-photo-133579-medium.jpeg",
+                "https://static.pexels.com/photos/51383/photo-camera-subject-photographer-51383-medium.jpeg",
+                "https://static.pexels.com/photos/205926/pexels-photo-205926-medium.jpeg",
+                "https://static.pexels.com/photos/2396/light-glass-lamp-idea-medium.jpg",
+                "https://static.pexels.com/photos/1854/person-woman-hand-relaxing-medium.jpg",
+                "https://static.pexels.com/photos/204611/pexels-photo-204611-medium.jpeg",
+                "https://static.pexels.com/photos/214487/pexels-photo-214487-medium.jpeg",
+                "https://static.pexels.com/photos/168575/pexels-photo-168575-medium.jpeg",
+                "https://static.pexels.com/photos/213384/pexels-photo-213384-medium.jpeg",
+                "https://static.pexels.com/photos/114907/pexels-photo-114907-medium.jpeg"
+        };
+        return urls;
+    }
+
+    public static String[] getLifeStyleUrls() {
+        String[] urls = new String[]{
+                "https://static.pexels.com/photos/169047/pexels-photo-169047-medium.jpeg",
+                "https://static.pexels.com/photos/160826/girl-dress-bounce-nature-160826-medium.jpeg",
+                "https://static.pexels.com/photos/1702/bow-tie-businessman-fashion-man-medium.jpg",
+                "https://static.pexels.com/photos/35188/child-childrens-baby-children-s-medium.jpg",
+                "https://static.pexels.com/photos/70845/girl-model-pretty-portrait-70845-medium.jpeg",
+                "https://static.pexels.com/photos/26378/pexels-photo-26378-medium.jpg",
+                "https://static.pexels.com/photos/193355/pexels-photo-193355-medium.jpeg",
+                "https://static.pexels.com/photos/1543/landscape-nature-man-person-medium.jpg",
+                "https://static.pexels.com/photos/211048/pexels-photo-211048-medium.jpeg",
+                "https://static.pexels.com/photos/189857/pexels-photo-189857-medium.jpeg",
+                "https://static.pexels.com/photos/157948/model-modelling-attractive-fashion-157948-medium.jpeg",
+                "https://static.pexels.com/photos/33608/dog-ice-woman-purple-medium.jpg",
+                "https://static.pexels.com/photos/157940/hair-bracelet-beautiful-beauty-157940-medium.jpeg",
+                "https://static.pexels.com/photos/35188/child-childrens-baby-children-s-medium.jpg",
+                "https://static.pexels.com/photos/70845/girl-model-pretty-portrait-70845-medium.jpeg",
+                "https://static.pexels.com/photos/26378/pexels-photo-26378-medium.jpg",
+                "https://static.pexels.com/photos/193355/pexels-photo-193355-medium.jpeg",
+        };
+        return urls;
+    }
+
+    public static String[] getBooksUrls() {
+        String[] urls = new String[]{
+                "https://static.pexels.com/photos/67442/pexels-photo-67442-medium.jpeg",
+                "https://static.pexels.com/photos/159494/book-glasses-read-study-159494-medium.jpeg",
+                "https://static.pexels.com/photos/33283/stack-of-books-vintage-books-book-books-medium.jpg",
+                "https://static.pexels.com/photos/205323/pexels-photo-205323-medium.png",
+                "https://static.pexels.com/photos/38167/pexels-photo-38167-medium.jpeg",
+                "https://static.pexels.com/photos/68562/pexels-photo-68562-medium.jpeg",
+                "https://static.pexels.com/photos/34592/pexels-photo-medium.jpg",
+                "https://static.pexels.com/photos/1579/hand-notes-holding-things-medium.jpg",
+                "https://static.pexels.com/photos/26890/pexels-photo-26890-medium.jpg",
+                "https://static.pexels.com/photos/67442/pexels-photo-67442-medium.jpeg",
+                "https://static.pexels.com/photos/159494/book-glasses-read-study-159494-medium.jpeg",
+                "https://static.pexels.com/photos/33283/stack-of-books-vintage-books-book-books-medium.jpg",
+                "https://static.pexels.com/photos/205323/pexels-photo-205323-medium.png",
+                "https://static.pexels.com/photos/38167/pexels-photo-38167-medium.jpeg",
+                "https://static.pexels.com/photos/68562/pexels-photo-68562-medium.jpeg",
+        };
+        return urls;
+    }
+
+    // Methods for Wishlist
+    public void addWishlistImageUri(String wishlistImageUri) {
+        this.wishlistImageUri.add(0,wishlistImageUri);
+    }
+
+    public void removeWishlistImageUri(int position) {
+        this.wishlistImageUri.remove(position);
+    }
+
+    public ArrayList<String> getWishlistImageUri(){ return this.wishlistImageUri; }
+
+    // Methods for Cart
+    public void addCartListImageUri(String wishlistImageUri) {
+        this.cartListImageUri.add(0,wishlistImageUri);
+    }
+
+    public void removeCartListImageUri(int position) {
+        this.cartListImageUri.remove(position);
+    }
+
+    public ArrayList<String> getCartListImageUri(){ return this.cartListImageUri; }
+}
